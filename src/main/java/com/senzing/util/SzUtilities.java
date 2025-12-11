6a1075ce5e924bc5228148e8d639308868ba7c08
Barry M. Caceres <barrycaceres@icloud.com>
2025-12-11 15:52:00 -0800

    Renamed SzSettingsUtilities to SzUtilities

--- src/main/java/com/senzing/util/SzUtilities.java
+++ src/main/java/com/senzing/util/SzUtilities.java
@@ -1,7 +1,19 @@
 package com.senzing.util;
 
+import static com.senzing.io.IOUtilities.UTF_8_CHARSET;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -9,10 +21,14 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 
+import com.senzing.sql.DatabaseType;
+import com.senzing.sql.SQLiteConnector;
+
 /**
- * Provides utilities for working with Senzing environment settings.
+ * Provides utilities for working with Senzing environment settings
+ * and repositories.
  */
-public class SzSettingsUtilities {
+public class SzUtilities {
     /**
      * The <b>unmodifiable</b> {@link Set} of legal prefixes for URI
      * prefixes for the database URI's for the Senzing repository.
@@ -23,7 +39,7 @@ public class SzSettingsUtilities {
     /**
      * Private default constructor.
      */
-    private SzSettingsUtilities() {
+    private SzUtilities() {
         // do nothing
     }
 
@@ -264,4 +280,150 @@ public class SzSettingsUtilities {
         }
     }
 
+    /**
+     * Ensures the Senzing schema is setup on the SQLite database accessible
+     * via the specified {@link Connection}.  This method will attempt
+     * to find the SQLite schema file at the following path: 
+     * <code>[resource-path]/schema/szcore-schema-sqlite-create.sql</code>
+     * directory.
+     * <p>
+     * The <code>[resource-path]</code> from the Senzing installation is 
+     * located using Java system properties, environment variables and
+     * defaults for the operating system with the following order of precedence:
+     * <ol>
+     *     <li><code>senzing.resource.dir</code> System Property</li>
+     *     <li><code>SENZING_RESOURCE_DIR</code> Environment Variable</li>
+     *     <li><code>[senzing-path]/resources</code> (see <code>[senzing-path]</code> below)</li>
+     * </ol>
+     * Where <code>[senzing-path]</code> is determined as:
+     * <ol>
+     *     <li><code>senzing.path</code> System Property</li>
+     *     <li><code>SENZING_PATH</code> Environment Variable</li>
+     *     <li>The default directory directory for the operating system:
+     *         <ul>
+     *             <li>Linux: <code>/opt/senzing</code></li>
+     *             <li>macOS: <code>$HOME/senzing</code></li>
+     *             <li>Windows: <code>%UserProfile%\senzing</code>
+     *         </ul>
+     *     </li>
+     * </ol>
+     * <p>
+     * <b>NOTE:</b> On Linux operating systems the <code>resources/schema</code>
+     * files are only present if you have installed the <code>senzingsdk-setup</code>
+     * package.  They are not available with the <code>senzingsdk-runtime</code>.
+     * 
+     * <p>
+     * <b>ALSO NOTE:</b> If this method determines that the Senzing SQLite 
+     * schema is already setup in the connected database, then this method will
+     * do nothing.
+     * 
+     * @param conn The non-null {@link Connection} to a SQLite database.
+     * 
+     * @return <code>true</code> if the schema was created, or <code>false</code>
+     *         if the schema was detected and no changes were made.
+     * 
+     * @throws SQLException If a JDBC failure occurs.
+     * 
+     * @throws NullPointerException If the specified {@link Connection} is
+     *                              <code>null</code>.
+     * 
+     * @throws IllegalArgumentException If the specified {@link Connection} is
+     *                                  not connected to a SQLite database.
+     * 
+     * @throws IllegalStateException If the schema file cannot be found.
+     */
+    public static boolean ensureSenzingSQLiteSchema(Connection conn) 
+        throws SQLException, IllegalStateException
+    {
+        Objects.requireNonNull(conn, "The connection cannot be null");
+        DatabaseType type = DatabaseType.detect(conn);
+        if (type != DatabaseType.SQLITE) {
+            throw new IllegalArgumentException(
+                "The specified Connection is not a SQLite connection: " 
+                + type);
+        }
+
+        // first find the file
+        SzInstallLocations installLocations = SzInstallLocations.findLocations();
+        
+        // get the resource directory
+        File resourceDir = installLocations.getResourceDirectory();
+        File schemaDir = new File(resourceDir, "schema");
+        if (!schemaDir.exists() || !schemaDir.isDirectory()) {
+            throw new IllegalStateException(
+                "Unable to find schema directory at: " + schemaDir);
+        }
+        File sqlFile = new File(schemaDir, "szcore-schema-sqlite-create.sql");
+        if (!sqlFile.exists() || !sqlFile.isFile()) {
+            throw new IllegalStateException(
+                "Unable to find schema file at: " + sqlFile);
+        }
+
+        // get the set of known tables
+        Set<String> knownTables = new HashSet<>();
+        String[] dbTypes = {"TABLE"};
+        DatabaseMetaData metaData = conn.getMetaData();
+        try (ResultSet rs = metaData.getTables(null, null, "%", dbTypes))
+        {
+            while (rs.next()) {
+                String tableName = rs.getString("TABLE_NAME");
+                knownTables.add(tableName.toUpperCase());
+            }
+        }
+
+        // now iterate through the file and get all the statements
+        // while looking for table names that already exist
+        List<String> statements = new LinkedList<>();
+        try (FileReader reader = new FileReader(sqlFile, UTF_8_CHARSET);
+                BufferedReader br = new BufferedReader(reader))
+        {
+            int lineNumber = 0;
+            for (String sql = br.readLine(); sql != null; sql = br.readLine())
+            {
+                lineNumber++;
+                // trim the line
+                sql = sql.trim();
+
+                // check if empty
+                if (sql.length() == 0) {
+                    continue;
+                }
+
+                // check if this is a CREATE TABLE statement
+                if (sql.toUpperCase().startsWith("CREATE TABLE ")) {
+                    // get the suffix and find the table name
+                    String suffix = sql.substring("CREATE TABLE ".length()).trim();
+                    int space = suffix.indexOf(" ");
+                    if (space < 0) {
+                        throw new IllegalStateException(
+                            "Failed to parse schema file at line (" + lineNumber + "): "
+                            + sql);
+                    }
+                    String tableName = suffix.substring(0, space).toUpperCase();
+
+                    // if the table already exists then cowardly refuse to proceed
+                    if (knownTables.contains(tableName)) {
+                        return false;
+                    }
+                }
+
+                // store the non-empty SQL statement so we can execute later
+                statements.add(sql);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException(
+                "Failed to read schema file: " + sqlFile, e);
+        }
+
+        // finally execute the statements
+        try (Statement stmt = conn.createStatement()) {
+            for (String sql : statements) {
+                stmt.execute(sql);
+            }
+        }
+        conn.commit();
+
+        // if we get here then we installed the schema
+        return true;
+    }
 }
